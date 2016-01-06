09c2ec7955308bd88429ba25e17bb20d0bbb7e8d
Sg4Dylan <dengbill123@gmail.com>
2016-01-06 21:39:25 +0800

    Init

---
+++ OBS_Music_Plugin.py
@@ -0,0 +1,48 @@
+import sys
+import time
+import string
+import psutil
+import ctypes
+from ctypes import wintypes
+import win32con
+import win32api
+import win32gui
+import win32process
+
+title = ""
+
+def enumWindowsProc(hwnd, lParam):
+    global title
+    if (lParam is None) or ((lParam is not None) and (win32process.GetWindowThreadProcessId(hwnd)[1] == lParam)):
+        text = win32gui.GetWindowText(hwnd)
+        if text:
+            wStyle = win32api.GetWindowLong(hwnd, win32con.GWL_STYLE)
+            if wStyle & win32con.WS_VISIBLE:
+                print("%08X - %s" % (hwnd, text))
+                title = text
+
+def enumProcWnds(pid=None):
+    win32gui.EnumWindows(enumWindowsProc, pid)
+
+def getProcName(procName):
+    for proc in psutil.process_iter():
+        if proc.name() == procName:
+            pid = str(proc)
+            rig = pid.split('=',3)
+            pid = rig[1]
+            rig = pid.split(',',2)
+            pid = rig[0]
+            return int(pid)
+
+def main(args):
+    while(1):
+        pid = getProcName(args[0])
+        enumProcWnds(pid)
+        #print title
+        fi = open("MusicTitle.txt","wb")
+        fi.write("Now on play: " + title)
+        fi.close()
+        time.sleep(2.0)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Show-now-playing-in-OBS
-You could use this script to read process title to text, and show your "now-playing" in OBS.
+You could use this script to read process title to text, and show your "now-playing" in OBS.  
+
+requirement:  psutil (you can install this from pip easily)  
+
+
+Using command  
+  python OBS_Music_Plugin.py "PROCESS_NAME"  
+to start  
+
+or you can use a shell to start this background.  
