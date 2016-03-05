6320b6aa475aacaded3f4242c9ce75eba29d1478
Sg4Dylan <dengbill123@gmail.com>
2016-03-05 10:07:17 +0800

    Add support for netease music

--- OBS_Music_Plugin.py
+++ OBS_Music_Plugin.py
@@ -29,6 +29,7 @@ def enumProcWnds(pid=None):
     win32gui.EnumWindows(enumWindowsProc, pid)
 
 def getProcName(procName):
+    pid_list = []
     for proc in psutil.process_iter():
         if proc.name() == procName:
             pid = str(proc)
@@ -36,18 +37,20 @@ def getProcName(procName):
             pid = rig[1]
             rig = pid.split(',',2)
             pid = rig[0]
-            return int(pid)
+            pid_list.append(pid)
+    return pid_list
 
 def main(args):
     while(1):
         pid = getProcName(args[0])
-        enumProcWnds(pid)
-        #print title
-        fi = open("MusicTitle.txt","wb")
-        title2 = "Now playing: " + title
-        title2 = title2.decode('gbk')
-        fi.write(title2.encode('utf-8'))
-        fi.close()
+        for x in range(0, len(pid)):
+            enumProcWnds(int(pid[x]))
+            if title != '':
+                fi = open("MusicTitle.txt","wb")
+                title2 = "Now playing: " + title + "   "
+                title2 = title2.decode('gbk')
+                fi.write(title2.encode('utf-8'))
+                fi.close()
         time.sleep(2.0)
 
 if __name__ == "__main__":
